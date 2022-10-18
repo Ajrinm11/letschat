bb02806a2e502fadf0ea9f12c6c985bfb5227371
Ajrinm11 <84498693+Ajrinm11@users.noreply.github.com>
2022-10-17 19:33:55 -0700

    Add files via upload

---
+++ letschatroom.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAWVRLRpaW-wJud-pDTpcB0h90iM8HX9XI",
+    authDomain: "letschat-9aec0.firebaseapp.com",
+    databaseURL: "https://letschat-9aec0-default-rtdb.firebaseio.com",
+    projectId: "letschat-9aec0",
+    storageBucket: "letschat-9aec0.appspot.com",
+    messagingSenderId: "289406138945",
+    appId: "1:289406138945:web:42f2fa03eb14d79926e4c0"
+  };
+
+  firebase.initializeApp(firebaseConfig);
