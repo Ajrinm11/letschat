9ecc07966ffdf21be5e0bd8999e5d0a93ea2571a
Ajrinm11 <84498693+Ajrinm11@users.noreply.github.com>
2022-10-22 18:40:27 -0700

    Add files via upload

--- letschatroom.js
+++ letschatroom.js
@@ -42,3 +42,20 @@ function redirectToRoomName(name)
   localStorage.setItem("room_name", name);
   window.location="letschatpage.html";
 }
+
+function logout(){
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+  window.location="index.html";
+}
+
+function send()
+{
+  msg = document.getElementById("msg").value;
+  firebase.database().ref(room_name).push({
+        name: user_name,
+        message:msg,
+        like:0
+  });
+  document.getElementById("msg").value="";
+} 
